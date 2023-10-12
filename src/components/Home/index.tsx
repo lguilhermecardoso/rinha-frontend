b4571e248d3e9c7a3499f41f7038536251a38d1f
Guilherme Cardoso <guilherme.cardoso@arcotech.io>
2023-10-12 17:42:08 -0300

    feat: first process and view json formated (simple viewer)

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,27 +1,60 @@
 import { useState } from "react"
+import { JsonTreeView } from "../JsonTreeView";
 
 export function HomeComponent() {
   const [hasError, setHasError] = useState(false)
+  const [jsonString, setJsonString] = useState('')
 
-  function handleUploadJson() {
-    setHasError(true)
+  function handleLoadJSON() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.json';
+    input.onchange = (event) => {
+      const file = (event.target as HTMLInputElement).files?.[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          try {
+            const json = JSON.parse(event.target?.result as string);
+            setJsonString(JSON.stringify(json, null, 2));
+            setHasError(false);
+          } catch (error) {
+            setHasError(true);
+          }
+        };
+        reader.readAsText(file);
+        console.log(file)
+      }
+    };
+    input.click();
   }
   return (
-    <div className="flex justify-center items-center h-[100vh]">
-      <div className="m-auto w-[100%] items-center flex flex-col">
-        <h1 className="text-5xl font-bold">
-          JSON Tree Viewer
-        </h1>
-        <h2 className="mt-6">Simple JSON Viewer that runs completely on-client. No data exchange </h2>
+    <>
+      {jsonString ? (
+        <>
+          <JsonTreeView json={jsonString} />
+        </>
+      ) : (
+        <>
+          <div className="flex justify-center items-center h-[100vh]">
+            <div className="m-auto w-[100%] items-center flex flex-col">
+              <h1 className="text-5xl font-bold">
+                JSON Tree Viewer
+              </h1>
+              <h2 className="mt-6">Simple JSON Viewer that runs completely on-client. No data exchange </h2>
 
-        <button onClick={handleUploadJson} className="border rounded-md border-solid border-black bg-gradient-to-b from-gray-300 to-gray-100 opacity-70 rounded-5 px-[12px] py-[6px] hover:opacity-20 transition mt-6">
-          Load JSON
-        </button>
-        {hasError && (
-          <p className="mt-6 font-light text-base text-[#BF0E0E]">Invalid file. Please load a valid JSON file.</p>
-        )}
-      </div>
+              <button onClick={handleLoadJSON} className="border rounded-md border-solid border-black bg-gradient-to-b from-gray-300 to-gray-100 opacity-70 rounded-5 px-[12px] py-[6px] hover:opacity-20 transition mt-6">
+                Load JSON
+              </button>
+              {hasError && (
+                <p className="mt-6 font-light text-base text-[#BF0E0E]">Invalid file. Please load a valid JSON file.</p>
+              )}
+            </div>
+          </div>
+        </>
+      )}
       
-    </div>
+    </>
+    
   )
 }
---
+++ src/components/JsonTreeView/index.tsx
@@ -0,0 +1,50 @@
+
+interface JsonTreeViewProps {
+  json: string
+}
+
+export function JsonTreeView({
+  json
+}: JsonTreeViewProps) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function formatJsonToTreeView(json: any, indent: number = 0, isLast: boolean = true): JSX.Element {
+    const indentString = ' '.repeat(indent * 4);
+    if (Array.isArray(json)) {
+      return (
+        <ul className="pl-4">
+          {json.map((item, index) => (
+            <li key={index}>
+              <span className="text-gray-500">{indentString}{index}: </span>
+              {formatJsonToTreeView(item, indent + 1, index === json.length - 1)}
+            </li>
+          ))}
+        </ul>
+      );
+    } else if (typeof json === 'object' && json !== null) {
+      return (
+        <ul className="pl-4">
+          {Object.entries(json).map(([key, value], index, array) => (
+            <li key={key}>
+              <span className="text-gray-500">{indentString}{key}: </span>
+              {formatJsonToTreeView(value, indent + 1, index === array.length - 1)}
+            </li>
+          ))}
+        </ul>
+      );
+    } else if (typeof json === 'string') {
+      return <span className="text-blue-500">"{json}"</span>;
+    } else if (typeof json === 'boolean') {
+      return <span className="text-green-500">{json ? 'true' : 'false'}</span>;
+    } else {
+      return <span className="text-blue-500">{json}</span>;
+    }
+  }
+
+  const treeView = formatJsonToTreeView(JSON.parse(json));
+
+  return (
+    <div className="bg-white rounded-md p-4">
+      {treeView}
+    </div>
+  );
+}
