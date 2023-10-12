92a52eda380d6841ba48def456f24bae5a235bd1
Guilherme Cardoso <guilherme.cardoso@arcotech.io>
2023-10-12 18:22:58 -0300

    feat: add new functions and css in aplication

--- src/components/JsonTreeView/index.tsx
+++ src/components/JsonTreeView/index.tsx
@@ -9,15 +9,17 @@ export function JsonTreeView({
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
   function formatJsonToTreeView(json: any, indent: number = 0, isLast: boolean = true): JSX.Element {
     console.log(isLast)
-    const indentString = ' '.repeat(indent * 4);
+    const indentString = ' '.repeat(indent * 2);
     if (Array.isArray(json)) {
       return (
-        <ul className="pl-4">
+        <ul className="pl-4 parent">
           {json.map((item, index) => (
-            <li key={index}>
-              <span className="text-gray-500">{indentString}{index}: </span>
-              {formatJsonToTreeView(item, indent + 1, index === json.length - 1)}
-            </li>
+            <div>
+              <li key={index}>
+                <span className="text-[#BFBFBF]">{indentString}{index}: </span>
+                {formatJsonToTreeView(item, indent + 1, index === json.length - 1)}
+              </li>
+            </div>
           ))}
         </ul>
       );
@@ -26,18 +28,20 @@ export function JsonTreeView({
         <ul className="pl-4">
           {Object.entries(json).map(([key, value], index, array) => (
             <li key={key}>
-              <span className="text-gray-500">{indentString}{key}: </span>
+              <span className="text-[#4E9590]">{indentString}{key}: </span>
               {formatJsonToTreeView(value, indent + 1, index === array.length - 1)}
             </li>
           ))}
         </ul>
       );
     } else if (typeof json === 'string') {
-      return <span className="text-blue-500">"{json}"</span>;
+      return <span className="text-gray-950">"{json}"</span>;
     } else if (typeof json === 'boolean') {
-      return <span className="text-green-500">{json ? 'true' : 'false'}</span>;
+      return <span className="text-gray-950">{json ? 'true' : 'false'}</span>;
+    } else if (json === null) {
+      return <span className="text-gray-950">null</span>;
     } else {
-      return <span className="text-blue-500">{json}</span>;
+      return <span className="text-gray-950">{json}</span>;
     }
   }
 
