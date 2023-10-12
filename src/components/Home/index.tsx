c61c58b830092ddd9b83ccf24147912da2659387
Guilherme Cardoso <guilherme.cardoso@arcotech.io>
2023-10-12 16:32:09 -0300

    feat: continue develop homepage

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react"
+
 export function HomeComponent() {
+  const [hasError, setHasError] = useState(false)
+
+  function handleUploadJson() {
+    setHasError(true)
+  }
   return (
     <div className="flex justify-center items-center h-[100vh]">
       <div className="m-auto w-[100%] items-center flex flex-col">
@@ -7,10 +14,14 @@ export function HomeComponent() {
         </h1>
         <h2 className="mt-6">Simple JSON Viewer that runs completely on-client. No data exchange </h2>
 
-        <button className="border border-solid border-black bg-gradient-to-b from-gray-300 to-gray-100 opacity-70 rounded-5 px-[12px] py-[6px] hover:opacity-20 transition mt-6">
+        <button onClick={handleUploadJson} className="border rounded-md border-solid border-black bg-gradient-to-b from-gray-300 to-gray-100 opacity-70 rounded-5 px-[12px] py-[6px] hover:opacity-20 transition mt-6">
           Load JSON
         </button>
+        {hasError && (
+          <p className="mt-6 font-light text-base text-[#BF0E0E]">Invalid file. Please load a valid JSON file.</p>
+        )}
       </div>
+      
     </div>
   )
 }
